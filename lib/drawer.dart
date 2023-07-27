7c63cf4c21307ada4aff51b0d2b412bcd345ef04
Simon <homer12345@gmail.com>
2023-07-26 21:20:31 -0700

    Switch to M3 NavigationDrawer

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -12,104 +12,102 @@ class MainDrawer extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = Theme.of(context);
+    final index = ref.watch(homepageProvider);
     const appicon = CircleAvatar(
       foregroundImage: AssetImage('assets/icon.png'),
     );
 
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: theme.colorScheme.primaryContainer,
+    return NavigationDrawer(
+      onDestinationSelected: (index) {
+        ref.read(homepageProvider.notifier).state =
+            HomePage.values.elementAt(index);
+      },
+      selectedIndex: index.index,
+      children: [
+        DrawerHeader(
+          decoration: BoxDecoration(
+            color: theme.colorScheme.primaryContainer,
+          ),
+          child: const Center(
+            child: Column(
+              children: [appicon, Text('Gagaku')],
             ),
-            child: const Center(
-              child: Column(
-                children: [appicon, Text('Gagaku')],
-              ),
-            ),
           ),
-          ListTile(
-            leading: const Icon(Icons.menu_book),
-            title: const Text('MangaDex'),
-            onTap: () {
-              ref.read(homepageProvider.notifier).state = HomePage.mangadex;
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.photo_album),
-            title: const Text('Local Library'),
-            onTap: () {
-              ref.read(homepageProvider.notifier).state = HomePage.local;
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.language),
-            title: const Text('Web Sources'),
-            onTap: () {
-              ref.read(homepageProvider.notifier).state = HomePage.web;
-            },
-          ),
-          FutureBuilder(
-            future: PackageInfo.fromPlatform(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return AboutListTile(
-                  icon: const Icon(Icons.info),
-                  applicationIcon: appicon,
-                  applicationName: snapshot.data!.appName,
-                  applicationVersion: snapshot.data!.version,
-                  applicationLegalese: '\u{a9} 2023 r52',
-                  aboutBoxChildren: [
-                    const SizedBox(
-                      height: 4,
+        ),
+        const NavigationDrawerDestination(
+          icon: Icon(Icons.menu_book_outlined),
+          selectedIcon: Icon(Icons.menu_book),
+          label: Text('MangaDex'),
+        ),
+        const NavigationDrawerDestination(
+          icon: Icon(Icons.photo_album_outlined),
+          selectedIcon: Icon(Icons.photo_album),
+          label: Text('Local Library'),
+        ),
+        const NavigationDrawerDestination(
+          icon: Icon(Icons.language_outlined),
+          selectedIcon: Icon(Icons.language),
+          label: Text('Web Sources'),
+        ),
+        const Divider(),
+        FutureBuilder(
+          future: PackageInfo.fromPlatform(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return AboutListTile(
+                icon: const Icon(Icons.info),
+                applicationIcon: appicon,
+                applicationName: snapshot.data!.appName,
+                applicationVersion: snapshot.data!.version,
+                applicationLegalese: '\u{a9} 2023 r52',
+                aboutBoxChildren: [
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  const Text('Flutter: $kFlutterFrameworkVersion'),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  const Text('Dart: $kFlutterDartSdkVersion'),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  const Text('Built on: $kBuildTimestamp'),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  const Text('License: MIT'),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        const TextSpan(text: 'Source code available at '),
+                        TextSpan(
+                          style: const TextStyle(color: Colors.blue),
+                          text: 'GitHub',
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              launchUrl(
+                                  Uri.parse('https://github.com/r52/gagaku'));
+                            },
+                        ),
+                        const TextSpan(text: '.'),
+                      ],
                     ),
-                    const Text('Flutter: $kFlutterFrameworkVersion'),
-                    const SizedBox(
-                      height: 4,
-                    ),
-                    const Text('Dart: $kFlutterDartSdkVersion'),
-                    const SizedBox(
-                      height: 4,
-                    ),
-                    const Text('Built on: $kBuildTimestamp'),
-                    const SizedBox(
-                      height: 4,
-                    ),
-                    const Text('License: MIT'),
-                    const SizedBox(
-                      height: 4,
-                    ),
-                    RichText(
-                      text: TextSpan(
-                        children: <TextSpan>[
-                          const TextSpan(text: 'Source code available at '),
-                          TextSpan(
-                            style: const TextStyle(color: Colors.blue),
-                            text: 'GitHub',
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                launchUrl(
-                                    Uri.parse('https://github.com/r52/gagaku'));
-                              },
-                          ),
-                          const TextSpan(text: '.'),
-                        ],
-                      ),
-                    ),
-                  ],
-                );
-              }
-
-              return const ListTile(
-                leading: Icon(Icons.info),
-                title: Text('About gagaku'),
+                  ),
+                ],
               );
-            },
-          ),
-        ],
-      ),
+            }
+
+            return const ListTile(
+              leading: Icon(Icons.info),
+              title: Text('About gagaku'),
+            );
+          },
+        ),
+      ],
     );
   }
 }
