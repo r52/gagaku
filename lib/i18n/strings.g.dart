62fb15adce035c7df218908781d1569996acc3c5
Simon <homer12345@gmail.com>
2025-12-11 10:32:14 -0800

    Update dependencies

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 1
 /// Strings: 435
 ///
-/// Built on 2025-11-27 at 17:04 UTC
+/// Built on 2025-12-11 at 18:17 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
