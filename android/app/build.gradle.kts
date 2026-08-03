8d6871859ce9bfaeb4b14dd9bf0cbe31a60e1bab
Simon <homer12345@gmail.com>
2026-08-03 02:02:53 -0700

    chore: up compileSdk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "r52.gagaku"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
